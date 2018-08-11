40cb8217a943066dfde234bab2be6122c17ee3bb
Li Ju <sheldon.juli@gmail.com>
2018-08-11 10:45:18 -0400

    Commited new project

---
+++ Score Board Donut/Score Board Donut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Score Board Donut
+//
+//  Created by Li Ju on 2018-08-11.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
